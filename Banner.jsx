d48af2859dc1d92ef7061017ca6011d70e783bd0
ArhamKhan <61708691+arham007@users.noreply.github.com>
2021-02-18 15:33:43 +0500

    Add files via upload

---
+++ Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Slider from './images/banner.png';
+
+const Banner=()=>{
+    return(
+        <>
+        <div>
+            <img src={Slider} alt="olx-banner" width="100%" height="220" />
+        </div>
+       
+        </>
+    )
+}
+
+export default Banner;
